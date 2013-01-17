00870cc402cdfeebe08c76a75179d5cd36219d59
Norbert Rab <hello@norb.es>
2013-01-17 12:49:33 +0100

    rails 3.2.1

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Humanex::Application.config.secret_token = '99c07fde7621a0c586ab4da81bb432b62267a0f3766dd42d264d95386f77da82ac44139ef8161192bf22fff11cf968859d4d96e94146e75f513bb3258c5e9908'
